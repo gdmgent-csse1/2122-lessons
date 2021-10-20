613408453472e39cb12092de6226b099babbad2c
Kristof Raes <krisra@arteveldehs.be>
2021-10-20 16:10:31 +0200

    Demo/exercises of Lesson04

---
+++ Lesson04/Exercise01/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static int Getal = 3;
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                
+                Console.WriteLine("Globale variabele Getal: " + Getal);
+                int getal = 4;
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                getal = GeefGetal();
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                getal = GeefGetal(getal);
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                getal = GeefGetal(getal);
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                GeefGetal(ref getal);
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                //throw new InvalidOperationException();
+                throw new SystemException();
+                GeefGetalByRef(ref getal);
+                Console.WriteLine("Lokale variabele Getal: " + getal);
+                int input1, input2;
+                GeefGetalOut(out input1, out input2);
+                //Console.WriteLine("Getal 1 en 2: getal1: " + input1 + " - getal2: " + input2);
+                Console.WriteLine("Getal 1 en 2: getal1: " + input2 + " - getal2: " + input1);
+                GeefGetalIn(input1);
+            }
+            catch (System.InvalidOperationException ioe)
+            {
+                // Stappen uitvoeren voor het geval InvalidOperationException.
+                Console.WriteLine("InvalidOperation EXCEPTION!!!");
+                Console.WriteLine(ioe.Message);
+            }
+            catch(System.SystemException se)
+            {
+                Console.WriteLine("SYSTEMEXCEPTION!!!");
+                Console.WriteLine(se.Message);
+                Console.WriteLine(se.StackTrace);
+            }
+            catch (System.Exception e)
+            {
+                // Stappen uitvoeren voor het geval Exception.
+                Console.WriteLine("BASE EXCEPTION!!!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+            }
+
+/*
+            Exception
+                - SystemException
+                    - InvalidOperationException
+                - ...
+
+ */           
+        }
+
+        static int GeefGetal()
+        {
+            return 6;
+        }
+        static int GeefGetal(int input)
+        {
+            Console.WriteLine("zonder ref");
+            return ++input;
+        }
+
+        static int GeefGetal(ref int input)
+        {
+            Console.WriteLine("met ref");
+            if (input > 2)
+            {
+                return input;
+            }
+            else
+            {
+                Console.WriteLine("test");
+                return 0;
+            }
+            //return ++input;
+            //return 9;
+        }
+
+        static void GeefGetalByRef(ref int input)
+        {
+            input = 10;
+            Console.WriteLine("met ref zonder return");
+        }
+
+        static void GeefGetalOut(out int getal2, out int getal1)
+        {
+            getal2 = 2;
+            getal1 = 1;
+            Console.WriteLine("met out");
+        }
+
+        static void GeefGetalIn(in int getal2)
+        {
+            int resultaat = getal2 + 5;
+            Console.WriteLine("met in");
+            Console.WriteLine("getal2: " + getal2 + "\nresultaat: "+ resultaat);
+        }
+
+
+    }
+}
---
+++ Lesson04/Exercise02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise02
+{
+    class Program
+    {
+        int getal = 5;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Product - Multiply.
+            // Tafel van vermenigvuldiging -> product.
+
+            int vermenigvuldigtal = Convert.ToInt32(Console.ReadLine());
+            // a x b
+            // a = vermenigvuldiger
+            // b = vermenigvuldigtal
+            int vermenigvuldiger = 2;
+            int resultaat = Product(vermenigvuldiger, vermenigvuldigtal);
+            Console.WriteLine("Resultaat product: " + resultaat);
+
+            TafelsVanVermenigvuldiging(vermenigvuldigtal);
+        }
+
+        static int Product(int a, int b)
+        {
+            /*
+                int resultaat = a * b;
+                return resultaat;
+            */
+            return a * b;
+        }
+
+        static void TafelsVanVermenigvuldiging(int b)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(""+i + " x " + b + " = " + i*b);
+            }
+
+            for (int j = 0; j < length; j++)
+            {
+                
+            }
+        }
+
+    }
+}
