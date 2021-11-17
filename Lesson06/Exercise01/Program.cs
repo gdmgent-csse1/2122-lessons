0a8ecbc200a658fae1346ee5c198df8c3454cf21
Kristof Raes <kristof.raes@arteveldehs.be>
2021-11-17 14:25:54 +0100

    Added folders lessons 6 & 7.

---
+++ Lesson06/Exercise01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int getal;
+            getal = 5;
+            getal += 3;
+            Console.WriteLine("Hello World! " + getal);
+            */
+
+            Console.WriteLine("Start demo Datacollecties...");
+
+            string[] merken = new string[5]{ "Coca-Cola", "Duvel", "Logitech", "Dell", "Apple" };
+           /*
+            int length = merken.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("merk"+i);
+                merken[i] = "merk" + i;
+            }
+            */
+
+            int length = merken.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(""+merken[i]);
+            }
+
+            string[,] computers = new string[3,2]{{"Dell", "3560"}, {"Apple", "MacBook M2"}, {"HP", "P954"}};
+            //int length1D = computers.Length;
+            //Console.WriteLine(length1D);
+            for (int i = 0; i < 3; i++)
+            {
+                
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("TEST FOR-FOR LOOPS:" + computers[i, j] + "-" +  computers[i, j]);
+                }
+            }
+
+            foreach (var item in computers)
+            {
+                Console.WriteLine("FOREACH" + item);
+            }
+
+           //string[,] 2darray = new string[3, 2] { { "BMW", "520" }, { "Golf", "1,9 TDI" },{ "Mercedes-Benz", "E 200" } };
+
+        }
+    }
+}
---
+++ Lesson07/Exercise01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Klassen...
+
+            // Voorbeeld van dieren...
+
+            /*
+            Voorbeeld van transportmiddel...
+            Een transportmiddel kan een voertuig (vervoermiddel met wielen of 
+            glijvlakken voor het vervoer over land van personen en goederen) 
+            zijn maar kan ook een vliegtuig zijn of een vaartuig.
+
+            Een voertuig kan dan ook weer een wagen zijn, motorfiets, fiets of vrachtwagen zijn...
+            Er is een relatie met voertuig en transportmiddel voor de onderliggende objectklassen wagen, motorfiets en fiets.
+            */
+        }
+    }
+
+    /* Access specifiers... 
+        private
+        public
+        protected
+    */
+
+
+    class Dier
+    {
+
+    }
+
+    class Zoogdier
+    {}
+
+    class Transportmiddel
+    {}
+
+    class Voertuig
+    {}
+
+    class Auto
+    {}
+
+    class Motorfiets
+    {}
+
+    class Fiets
+    {}
+
+    class Vrachtwagen
+    {}
+}
